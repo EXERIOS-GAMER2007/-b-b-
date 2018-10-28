0675f3962db17019a2f84955ea33807a361fded2
VEKKX <44268685+EXERIOS-GAMER2007@users.noreply.github.com>
2018-10-28 09:22:03 +0300

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("452592552135360524")
+setInterval(function() {
+channel.send(`youtube`);
+}, 25)
+})
+ 
+ 
+client.login('NDk4MDQ1OTk3NjI1Mzc2Nzc4.DrblMA.BeiclLLaa18ZpdDeLD7Xl81QIBc');
